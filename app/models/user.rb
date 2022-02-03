32c71abf4d01da9042f8eee51c8fbf8559916bb6
KlimSemikin <klimsemikin@gmail.com>
2022-02-03 16:46:57 +0600

    Add user model with auth

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  extend Devise::Models
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  validates :email, length: { maximum: 100 }
+end
