5a348b5e14efd8cd8e2cdf304345a091c5ca62fc
KlimSemikin <klimsemikin@gmail.com>
2022-02-03 19:35:55 +0600

    Add tests for column

--- test/models/column_test.rb
+++ test/models/column_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class ColumnTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.create(email: "user@example.com",
+                        password: 'foobar',
+                        password_confirmation: 'foobar')
+    @column = Column.new(user: @user, title: 'good title')
+  end
+
+  test "should be valid" do
+    assert @column.valid?
+  end
+
+  test "should be invalid, by wrong user_id" do
+    @column.user_id = 2
+    assert_not @column.valid?
+  end
+
+  test "title should be not null" do
+    @column.title = nil
+    assert_not @column.valid?
+  end
+
+  test "title should be present" do
+    @column.title = "     "
+    assert_not @column.valid?
+  end
+
+  test "title should not be too long" do
+    @column.title = "a" * 513
+    assert_not @column.valid?
+  end
 end
