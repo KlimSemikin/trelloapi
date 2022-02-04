2ef886faa3bf2ae9ebe345f3e3970085fa58c342
KlimSemikin <klimsemikin@gmail.com>
2022-02-04 17:43:08 +0600

    Add faker and fake content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  user = User.create({ email: Faker::Internet.email, password: Faker::Internet.password })
+  column = user.columns.create({ title: Faker::Vehicle.engine })
+  card = column.cards.create({ title: Faker::Adjective.positive, user: user })
+  card.comments.create({ content: Faker::Lorem.sentence, user: user })
+  card.comments.create({ content: Faker::Lorem.sentence, user: user })
+  card2 = column.cards.create({ title: Faker::Vehicle.transmission, user: user })
+  card2.comments.create({ content: Faker::Lorem.sentence, user: user })
+  card2.comments.create({ content: Faker::Lorem.sentence, user: user })
+end
